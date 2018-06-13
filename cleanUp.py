adee7b79fb01e3ca7e44dd749b14576ffa515733
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-06-13 03:02:54 +0000

    Update cleanUp.py

--- cleanUp.py
+++ cleanUp.py
@@ -1,34 +1,36 @@
 import pygments.token
 import pygments.lexers
-from difflib import SequenceMatcher
 
 def tokenize(filename):
     file=open(filename, "r")
     text=file.read()
+    file.close()
     lexer = pygments.lexers.guess_lexer_for_filename(filename, text)
     tokens = lexer.get_tokens(text)
-    tokens=list(tokens)
-    result=[]
-    lenT=len(tokens)
+    tokens = list(tokens)
+    result = []
+    lenT = len(tokens)
+    count1 = 0
+    count2 = 0
     for i in range(lenT):
         if tokens[i][0] == pygments.token.Name and not i == lenT-1 and not tokens[i+1][1] == '(':
-            result.append('N')
+            result.append(('N', count1, count2))
+            count2 += 1
         elif tokens[i][0] in pygments.token.Literal.String:
-            result.append('S')
-        elif not i == lenT-1 and tokens[i+1][1]=='(' and not tokens[i][0] in pygments.token.Keyword:
-            result.append('F')
+            result.append(('S', count1, count2))
+            count2 += 1
+        elif tokens[i][0] in pygments.token.Name.Function:
+            result.append(('F', count1, count2))
+            count2 += 1
         elif tokens[i][0] == pygments.token.Text or tokens[i][0] in pygments.token.Comment:
             pass
         else:
-            result.append(tokens[i][1])
+            result.append((tokens[i][1], count1, count2))
+            count2 += len(tokens[i][1])
+        count1 += len(tokens[i][1])
 
-    cleanText=''.join(str(x) for x in result)
+    return result
+
+def toText(arr):
+    cleanText=''.join(str(x[0]) for x in arr)
     return cleanText
-
-def plagerised_ratio(file1,file2):
-    file1=tokenize(file1)
-    file2=tokenize(file2)
-    print(file1)
-    print(file2)
-    similarity_ratio = SequenceMatcher(None,file1,file2).ratio()
-    print (similarity_ratio)# required ratio of plagerise
