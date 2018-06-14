c3365ce022dace2fe7b747c871c6614244b16a54
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-06-14 20:17:58 +0530

    Update cleanUP.py

--- cleanUP.py
+++ cleanUP.py
@@ -1,6 +1,9 @@
 import pygments.token
 import pygments.lexers
 
+#Python module pygments is used to tokenize the code files. This module supports most of the popular languages
+#http://pygments.org/languages/
+#Hence this program can be used to clean up codes written in most languages
 def tokenize(filename):
     file = open(filename, "r")
     text = file.read()
@@ -10,22 +13,24 @@ def tokenize(filename):
     tokens = list(tokens)
     result = []
     lenT = len(tokens)
-    count1 = 0
-    count2 = 0
+    count1 = 0    #tag to store corresponding position of each element in original code 
+    count2 = 0    #tag to store position of each element in cleaned up code
+    # these tags are used to mark the plagiarized content in the original text files.
     for i in range(lenT):
         if tokens[i][0] == pygments.token.Name and not i == lenT - 1 and not tokens[i + 1][1] == '(':
-            result.append(('N', count1, count2))
+            result.append(('N', count1, count2))  #all variable names as 'N'
             count2 += 1
         elif tokens[i][0] in pygments.token.Literal.String:
-            result.append(('S', count1, count2))
+            result.append(('S', count1, count2))  #all strings as 'S'
             count2 += 1
         elif tokens[i][0] in pygments.token.Name.Function:
-            result.append(('F', count1, count2))
+            result.append(('F', count1, count2))   #user defined function names as 'F'
             count2 += 1
         elif tokens[i][0] == pygments.token.Text or tokens[i][0] in pygments.token.Comment:
-            pass
+            pass   #whitespaces and comments ignored
         else:
-            result.append((tokens[i][1], count1, count2))
+            result.append((tokens[i][1], count1, count2))  
+            #tuples in result - (each element e.g 'def', its position in original text file, position in cleaned up text) 
             count2 += len(tokens[i][1])
         count1 += len(tokens[i][1])
 
